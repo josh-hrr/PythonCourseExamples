ca79d0b888fcab8ef089852ce7fa24bef5066d17
Josué Herrera <54568984+josh-hrr@users.noreply.github.com>
2022-12-07 21:49:42 +0000

    added #sandwich challenge

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ for item in myList:
   
 #dictionary 
   
-range = range(5)  
+#range = range(5)  
 newId = 0 
 myDic = {1: 'Jhon', 2: 'New', 3: 'Test'} 
 k = myDic.keys()
@@ -82,7 +82,40 @@ print(myNewList)
 myNewList.insert(2, 'Random')
 print(myNewList)
 
+b = 5.1//2
+print(b)
+
+height = 6.2
+heightInMeters = height * 0.0254
+print('\ntesting how to separate output')
+print(heightInMeters, '\nTesting: ', height)
+
+#formatting float into double
+testingFloat = 5.58821
+print('%.2f'%(testingFloat))
+
+#sandwich challenge
 
 
 
+toppingsList = ['Onions', 'Lettuce', 'Tomato', 'Olives']
+chosenList = []
+
+print("Toppings available: ", toppingsList)
+
+myRange = range(3) 
+for i in myRange: 
+  result = input('Enter topping #' + str(i + 1) + ': ')
+  for k in toppingsList:
+    if(result == k):
+      chosenList.append(result)  
+
+result2 = input('Enter how many you would like: ')
+counter = 0  
+
+if(int(result2) > 0):
+  counter = int(result2) * 5
+
+print("Toppings enter: ", chosenList, " Note: if you do not see the toppings enter it means you did not choose the ones listed at the beginning therefore they do not exist")
+print("Total: ", " $", counter)
 
